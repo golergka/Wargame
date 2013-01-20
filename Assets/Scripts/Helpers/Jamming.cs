b361a0a743c7a872bc375b3c0463a0aaf431a1cb
Max Yankov <golergka@gmail.com>
2013-01-20 18:47:47 +0400

    Closed #1 Arrow attacks

--- Assets/Scripts/Helpers/Jamming.cs
+++ Assets/Scripts/Helpers/Jamming.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(ParameterManager))]
 public class Jamming : MonoBehaviour {
 
 	bool jammed = false;
 
 	public List<Transform> exceptions = new List<Transform>();
 
-	void OnCollisionEnter(Collision collision) {
+	ParameterManager parameterManager;
+
+	int damage {
+		get {
+			return parameterManager.GetParameterValue<int>(Attack.DAMAGE_KEY);
+		}
+	}
+
+	void Awake() {
+
+		parameterManager = GetComponent<ParameterManager>();
+
+	}
+
+	void Jam(Transform other) {
 
 		if (jammed)
 			return;
 
-		if (exceptions.Contains(collision.transform))
+		if (exceptions.Contains(other))
+			return;
+
+		if (other.collider.isTrigger)
 			return;
 
 		jammed = true;
@@ -20,27 +38,23 @@ public class Jamming : MonoBehaviour {
 		if (rigidbody != null)
 			rigidbody.isKinematic = true;
 
-		transform.parent = collision.transform;
+		Health health = other.GetComponent<Health>();
+		if (health != null)
+			health.InflictDamage(damage, this);
+
+		transform.parent = other;
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		Jam(collision.transform);
 
 	}
 
 	void OnTriggerEnter(Collider other) {
 
-		if (!collider.isTrigger)
-			return;
-
-		if (jammed)
-			return;
-
-		if (exceptions.Contains(other.transform))
-			return;
-
-		jammed = true;
-
-		if (rigidbody != null)
-			rigidbody.isKinematic = true;
-
-		transform.parent = other.transform;
+		Jam(other.transform);
 
 	}
 
